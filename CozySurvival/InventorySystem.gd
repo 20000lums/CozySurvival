d837765956f0699657ae15c297721a500c454fcc
20000lums <jm686sc@gmail.com>
2023-10-16 11:58:48 -0700

    switch item func created

--- CozySurvival/InventorySystem.gd
+++ CozySurvival/InventorySystem.gd
@@ -1,17 +1,40 @@
 extends Node2D
+class Item:
+	func _init(stacksize):
+		self.stacksize = stacksize
+var InventoryOpen = false
+var Empty = Item.new(0)
+var PassPoint 
+var Inventory = [Empty,Empty,Empty,Empty,Empty]
+var InHand
+var Selected
+func Item_Switch(slot):
+	PassPoint = Inventory[slot]
+	Inventory[slot] = InHand
+	InHand = PassPoint
 
-
-
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Button_pressed():
+	if(InventoryOpen):
+		Item_Switch(0)
+	else:
+		Selected = 0
+func _on_Button2_pressed():
+	if(InventoryOpen):
+		Item_Switch(1)
+	else:
+		Selected = 1
+func _on_Button3_pressed():
+	if(InventoryOpen):
+		Item_Switch(2)
+	else:
+		Selected = 2
+func _on_Button4_pressed():
+	if(InventoryOpen):
+		Item_Switch(3)
+	else:
+		Selected = 3
+func _on_Button5_pressed():
+	if(InventoryOpen):
+		Item_Switch(4)
+	else:
+		Selected = 4
